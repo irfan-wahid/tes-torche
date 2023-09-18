cf9fe317527ab046650508939127ab93e1301d10
irfan-wahid <muhammad.irfan.tif20@polban.ac.id>
2023-09-18 22:41:39 +0700

    user crud api

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function register(Request $request){
+        $validated = $request->validate([
+            'email' => ['string', 'required', 'email', 'max:255'],
+            'password' => ['string', 'required', 'max:255'],
+            'name' => ['string', 'required', 'max:255'],
+        ]);
+
+        $user = new User($validated);
+        $user->password = Hash::make($validated['password']);
+        $user->save();
+
+        if ($user) {
+            return response()->json([
+                'message' => 'success',
+                'data' => $user,
+            ]);
+        } else {
+            return response()->json([
+                'message' => 'Gagal membuat akun',
+            ]
+            );
+        }
+    }
+
+    public function login(Request $request){
+        $validated = $request->validate([
+            'email' => ['string', 'required', 'email', 'max:255'],
+            'password' => ['string', 'required', 'max:255']
+        ]);
+
+        $user = User::where('email', $validated['email'])->first();
+        if(!$user || !Hash::check($validated['password'], $user->password)){
+            throw new HttpResponseException(response([
+                "errors"=>[
+                    "message" => [
+                        "Username atau password salah"
+                    ]
+                ]
+            ], 401));
+        }
+
+        return response()->json([
+            'message' => 'success',
+            'data' => $user,
+        ]);
+    }
+
+    public function changePassword(Request $request){
+        $validated = $request->validate([
+            'email' => ['string', 'required', 'email', 'max:255'],
+            'password' => ['string', 'required', 'max:255']
+        ]);
+
+        $user = User::where('email', $validated['email'])->first();
+
+        if(!$user){
+            throw new HttpResponseException(response([
+                "errors"=>[
+                    "message" => [
+                        "Akun tidak ditemukan"
+                    ]
+                ]
+            ], 404));
+        }
+
+        $user->password = Hash::make($validated['password']);
+        $user->save();
+
+        return response()->json([
+            'message' => 'Password berhasil diubah'
+        ]);
+    }
+
+    public function delete(Request $request){
+        $validated = $request->validate([
+            'email' => ['string', 'required', 'email', 'max:255']
+        ]);
+
+        $user = User::where('email', $validated['email'])->first();
+
+        if(!$user){
+            throw new HttpResponseException(response([
+                "errors"=>[
+                    "message" => [
+                        "Akun tidak ditemukan"
+                    ]
+                ]
+            ], 404));
+        }
+
+        $user->delete();
+
+        return response()->json([
+            'message' => 'Akun berhasil dihapus'
+        ]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::post('/register', [\App\Http\Controllers\UserController::class, 'register']);
+Route::get('/login', [\App\Http\Controllers\UserController::class, 'login']);
+Route::put('/changePassword', [\App\Http\Controllers\UserController::class, 'changePassword']);
+Route::delete('/delete', [\App\Http\Controllers\UserController::class, 'delete']);
